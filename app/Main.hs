5af554b7b9b1cfbfce6ae05bdd4a516c22d28a73
Seth Speaks <sspeaks@microsoft.com>
2019-06-04 20:26:28 +0000

    vscode docker container

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,15 @@
 module Main where
 
-import 			 Lib
+import           Fours
 import           System.Environment
-import			 System.Exit
-import 			 Data.List
-import 			 Control.Monad
+import           System.Exit
+import           Data.List
+import           Control.Monad
 
 main :: IO ()
 main = do
-	ls <- (liftM (map read) $ getArgs)::IO [Int]
-	if (length ls) < 2
-		then die "You must provide atleast 3 numbers"
-		else putStrLn . unlines $ solve (init ls) (fromIntegral $ last ls)
-				  
+    ls <- fmap (map read) getArgs :: IO [Float]
+    if length ls < 2
+        then die "You must provide atleast 3 numbers"
+        else putStrLn . unlines $ prettyAnswer (init ls) (last ls)
+
--- src/fours.hs
+++ src/fours.hs
@@ -1,104 +1,56 @@
-Module Fours
-    ( solve
-    ) where
+module Fours (prettyAnswer) where
+import Control.Applicative
+import Control.Monad
+import Data.List
+import Data.Maybe
 
-import           Control.Applicative hiding (many, (<|>))
-import           Control.Monad
-import           Data.List
-import           Text.Parsec
-import           Text.Parsec.Char
-import           Text.Parsec.String
+data Op = Multiply | Divide | Add | Subtract
+data Expr = Val Float | Tree Op Expr Expr
 
-opss = "*/+-"
-ops = map (:[]) opss
+evalExpr (Val x) = Just x
+evalExpr (Tree o l r) = case o of
+    Multiply -> liftM2 (*) left right
+    Add -> liftM2 (+) left right
+    Subtract -> liftM2 (-) left right
+    Divide -> case right of
+        (Just 0) -> Nothing
+        _ -> liftM2 (/) left right
+  where left = evalExpr l
+        right = evalExpr r
 
-interleave:: [String] -> [String] -> [String]
-interleave [x] _ = [x]
-interleave xs [] = xs
-interleave xss@(x:xx:xs)(y:ys)
-        | x == "(" || xx == ")" = x : interleave (xx:xs) (y:ys)
-        | otherwise = x : y : interleave (xx:xs) ys
+instance Show Expr where
+    show (Val x) = show x
+    show t@(Tree o l r) = case o of
+        Multiply -> "(" ++ show l ++ "*" ++ show r ++ ")"
+        Add -> "(" ++ show l ++ "+" ++ show r ++ ")"
+        Subtract -> "(" ++ show l ++ "-" ++ show r ++ ")"
+        Divide -> "(" ++ show l ++ "/" ++ show r ++ ")"
 
-parenthesize:: (Show a) => [a] -> [a] -> [[String]]
-parenthesize [] [] = []
-parenthesize [] [x] = [[show x]]
-parenthesize [x] [] = [[show x]]
-parenthesize [] (x:xs) = parenthesize [x] xs
-parenthesize xs [] = []
-parenthesize xss@(x:xs) yss@(y:ys) = ((++) <$> parens xss <*> parens yss) ++ next
-    where parens zs
-            | length zs == 1 = parenthesize [] zs
-            | otherwise = map addParens . parenthesize [] $ zs
-          next = parenthesize ((x:xs) ++ [y]) ys
+splits xs = init . tail $ zip (inits xs) (tails xs)
 
-addParens s = ["("] ++ s ++ [")"]
+combos l r = [id] <*> (Tree <$> [Add, Subtract, Multiply, Divide] <*> [l] <*> [r])
 
-sequenceN = replicateM
+trees:: [Float] -> [Expr]
+trees [x] = [id] <*> [Val x]
+trees xs = do
+    (f, s) <- splits xs
+    l <- trees f
+    r <- trees s
+    combos l r
 
---List of numbers to choose from -> number of numbers -> all eqs
-allEqs:: [Int] -> Int -> [String]
-allEqs xs n = map concat (interleave <$> parens <*> nOps)
-      where parens = concatMap (parenthesize []) . sequenceN n $ xs
-            nOps = sequenceN (n-1) ops
+results :: [Float] -> IO ()
+results xs = putStrLn . unlines $ zipWith (\x y  -> show x ++ "=" ++ show y) validTrees results
+    where validTrees = mfilter (isJust . evalExpr) $ trees xs
+          results =  mapMaybe evalExpr $ trees xs
 
---List of specific numbers -> all Eqs
-specAllEqs:: [Int] -> [String]
-specAllEqs xs = map concat (interleave <$> parens <*> nOps)
-      where parens = concatMap (parenthesize []) . nub . permutations $ xs
-            nOps = sequenceN (length xs - 1) ops
+isAnswer :: Float -> Maybe Float -> Bool
+isAnswer _ Nothing = False
+isAnswer y (Just x) = x == y
+       
+answer :: [Float] -> Float -> [Expr]
+answer xs x = mfilter (\v -> isAnswer x (evalExpr v)) $ trees xs
 
-specEqs:: [Int] -> [String]
-specEqs xs = map concat (interleave <$> parenthesize [] xs <*> nOps)
-        where nOps = sequenceN (length xs - 1) ops
-
-num::Parser Float
-num = do
-    spaces
-    x <- oneOf ['1'..'9']
-    xs <- many . oneOf $ ['0'..'9']
-    spaces
-    return $ read (x:xs)
-
-tChar:: Char -> Parser String
-tChar c = do
-           spaces
-           x <- char c
-           spaces
-           return [x]
-
-factor::Parser Float
-factor = (do
-            tChar '('
-            val <- expr
-            tChar ')'
-            return val) <|> num
-
-component = do x <- factor
-               (do tChar '*'
-                   y <- component
-                   return $ x * y) <|> (do tChar '/'
-                                           y <- component
-                                           return $ x / y) <|> return x
-expr = do x <- component
-          (do tChar '+'
-              y <- expr
-              return $ x + y) <|> (do tChar '-'
-                                      y <- expr
-                                      return $ x - y) <|> return x
-
-evalList [] = []
-evalList (x:xs) = case eval x of
-                    (Right n) -> (x,n):evalList xs
-                    (Left _)  -> evalList xs
-
-
-
-eval = parse expr []
-
-
-
-
-solve:: [Int] -> Float -> [String]
-solve xs x = map (\(a,b) -> a ++ " = " ++ show b) . filter (\(a,b) -> b == x) . evalList . specAllEqs $ xs
-
-main = putStr "hello world"
+prettyAnswer :: [Float] -> Float -> [String]
+prettyAnswer xs x = map (\exp -> show exp ++ "=" ++ show x) $ res
+    where res = answer xs x
+    
